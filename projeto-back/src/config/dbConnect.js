13da74a667d2a77a7da39a7ed0b26a44374ee244
LucasSilva1207 <lucas.luc1207@gmail.com>
2024-06-06 16:05:22 -0300

    atualizandoo

--- projeto-back/src/config/dbConnect.js
+++ projeto-back/src/config/dbConnect.js
@@ -1,26 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config(); // Carrega as variáveis do arquivo .env
 
-async function connectDataBase() {
-    const mongoUrl = process.env.MONGO_URL;
-    if (!mongoUrl) {
-        throw new Error("A variável de ambiente MONGO_URL não está definida.");
-    }
-
-    try {
-        await mongoose.connect(mongoUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Conexão com o MongoDB estabelecida com sucesso.");
-    } catch (error) {
-        console.error("Erro ao conectar com o MongoDB:", error);
-        throw error;
-    }
+async function connectDataBase(){
+    mongoose.connect("mongodb+srv://lucasluc1207:87500273@cluster0.li8djjr.mongodb.net/adotaeu?retryWrites=true&w=majority");
 
     return mongoose.connection;
 }
 
 export default connectDataBase;
+
+    
